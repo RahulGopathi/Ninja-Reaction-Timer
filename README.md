cec56e05f8f8a9f80d6366f5809562a918b069ea
Rahul Gopathi <75736709+RahulGopathi@users.noreply.github.com>
2021-06-28 09:08:42 +0530

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# Ninja-Reaction-Timer
-a small game with Vue3
+# t
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
+
+### Compiles and minifies for production
+```
+yarn build
+```
+
+### Lints and fixes files
+```
+yarn lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/components/Block.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="block" v-if="showBlock" @click="stopTimer">Click Me</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showBlock: false,
+      timer: null,
+      reationTime: null,
+    };
+  },
+  mounted() {
+    setTimeout(() => {
+      this.showBlock = true;
+      this.startTimer();
+    }, this.delay);
+  },
+  props: ["delay"],
+  methods: {
+    startTimer() {
+      this.timer = setInterval(() => {
+        this.reationTime += 10;
+      }, 10);
+    },
+    stopTimer() {
+      clearInterval(this.timer);
+      this.$emit("end", this.reationTime);
+    },
+  },
+};
+</script>
+
+<style>
+.block {
+  width: 400px;
+  border-radius: 20px;
+  background: #0faf87;
+  color: white;
+  text-align: center;
+  padding: 100px 0px;
+  margin: 40px auto;
+}
+</style>
